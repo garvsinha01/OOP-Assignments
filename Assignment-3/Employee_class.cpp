e0efa34ef4b28ff9aa7d0d69137c637d2abd6965
garvsinha01 <garvsinha01@gmail.com>
2026-09-11 22:33:38 +0530

    Update Employee_class.cpp

--- Assignment-3/Employee_class.cpp
+++ Assignment-3/Employee_class.cpp
@@ -1,56 +1,33 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-class Employee {
-
-private:
-
-  int Employee_ID;
-  string name;
-  string department;
-  string role;
-  float salary;
+class Employ {
+    string name;
+    int id;
+    string department;
+    float salary;
 
 public:
+    void input() {
+        name = "Garv";
+        id = 101;
+        department = "HR";
+        salary = 35000;
+    }
 
-   void input() {                     
-     cout << "Enter Employee ID: ";
-     cin >> Employee_ID;
-
-     cin.ignore();                     // using this or the outoput becomes in such a way that there is no next line after "Enetr Name:".
-
-     cout << "Enter Employee Name: ";   
-     getline(cin, name);                // I am using getline instead of cin as cin only prints the first word.
-
-     cout << "Enter Department: ";
-     getline(cin, department);
-
-     cout << "Enter Role: ";
-     getline(cin, role);
-
-     cout << "Enter Salary: ";
-     cin >> salary;
-
-   }
-
-   void display() {                   
-      cout << "\n-----Employee Details-----" << endl;
-      cout << "Employee ID    : "  << Employee_ID << endl;
-      cout << "Name           : "  << name << endl;
-      cout << "Department     : "  << department << endl;
-      cout << "Salary         : "  << salary << endl;
-   }
+    void display() {
+        cout << "Employee Name: " << name << endl;
+        cout << "Employee ID: " << id << endl;
+        cout << "Department: " << department << endl;
+        cout << "Salary: Rs. " << salary << endl;
+    }
 };
 
 int main() {
-  Employee emp;                      
+    Employ e;
 
-  cout << "Enter Employee Information\n";
-  emp.input();
+    e.input();
+    e.display();
 
-  cout << "\nAuthorized Staff - Viewing Employee Information";
-  emp.display();
-
-  return 0;
+    return 0;
 }

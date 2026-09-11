5b96ec479892e8e2b0ca241517c4bae9e66dd2a9
garvsinha01 <garvsinha01@gmail.com>
2026-09-11 22:07:27 +0530

    Create Student_Class.cpp

---
+++ Assignment-2/Student_Class.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student {
+private:
+    int name;
+    string rollno;
+    string course;
+    float marks;
+
+public:
+    void input() {
+        name = "Garv Sinha;
+        rollno = "32";
+        course = "CSE-AIML";
+        marks = 89.5;
+    }
+
+    void display() {
+        cout << "Name : " << name << endl;
+        cout << "Roll No   : " << rollno << endl;
+        cout << "Course  : " << course << endl;
+        cout << "Marks   : " << marks << endl;
+    }
+};
+
+int main()
+{
+    Student s;
+    s.input();
+    s.display();
+
+    return 0;
+}

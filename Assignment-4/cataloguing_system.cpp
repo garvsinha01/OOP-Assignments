992c14d8aac30c32cd799a720dc4ea41c5c13a4d
garvsinha01 <garvsinha01@gmail.com>
2026-09-11 22:17:12 +0530

    Create cataloguing_system.cpp

---
+++ Assignment-4/cataloguing_system.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Book {
+    string title;
+    string author;
+    int price;
+
+public:
+    Book() {
+        title = "ABC";
+        author = "DEF";
+        price = 0;
+    }
+
+    Book(string t, string a, int p) {
+        title = t;
+        author = a;
+        price = p;
+    }
+
+    void display() {
+        cout << "Book Title: " << title << endl;
+        cout << "Author: " << author << endl;
+        cout << "Price: Rs. " << price << endl;
+    }
+};
+
+int main() {
+    Book b1;
+
+    Book b2("OOP", "XYZ", 350);
+
+    cout << "Book 1:" << endl;
+    b1.display();
+
+    cout << "\nBook 2:" << endl;
+    b2.display();
+
+    return 0;
+}
+```

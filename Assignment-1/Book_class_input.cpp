63db0f95c87e1279f0b58f05c29dc8ac28b937a2
garvsinha01 <garvsinha01@gmail.com>
2026-09-11 22:10:22 +0530

    Update Book_class_input.cpp

--- Assignment-1/Book_class_input.cpp
+++ Assignment-1/Book_class_input.cpp
@@ -1,52 +1,29 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 class Book {
-private:
-
-   int book_id;
-   string name;
-   string author;
-   string publisher;
-   float price;
+    string title;
+    string author;
+    int price;
 
 public:
+    Book(string t, string a, int p) {
+        title = t;
+        author = a;
+        price = p;
+    }
 
-  void input() {
-    cout << "Enter Book ID: ";
-    cin >> book_id;
-    cin.ignore();
-
-    cout << "Enter Name: ";
-    getline(cin, name);     // using getline to print first 4 as it was not entering the next line to print.
-
-    cout << "Enter Author: ";
-    getline(cin, author);
-
-    cout << "Enter Publisher: ";
-    getline(cin, publisher);
-
-    cout << "Enter Price: ";
-    cin >> price;
-  }
-
-  void display() {
-    cout << "\n-----Book Details-----" << endl;
-    cout << "Book ID : " << book_id << endl;
-    cout << "Name : " << name << endl;
-    cout << "Author : " << author << endl;
-    cout << " Publisher : " << publisher << endl;
-  }
+    void input() {
+        cout << "Book Title: " << title << endl;
+        cout << "Author: " << author << endl;
+        cout << "Price: Rs. " << price << endl;
+    }
 };
 
 int main() {
-  Book book;
+    Book b("OOP", "XYZ", 350);
 
-  cout << "Enter Book Information:- " << endl;
-  book.input();
+    b.input();
 
-  cout << "Book Staff Viewing the Information.";
-  book.display();
-  return 0;
+    return 0;
 }
